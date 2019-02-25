f7a65bccba3151d459044d3e577230c686e36cb7
ddionisio <garapata@gmail.com>
2019-02-25 16:43:59 -0500

    initial dialog art

---
+++ Assets/Scripts/Game/TestUIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUIController : GameModeController<TestUIController> {
+    protected override void OnInstanceInit() {
+        base.OnInstanceInit();
+    }
+
+    protected override IEnumerator Start() {
+        yield return base.Start();
+
+
+    }
+}
