e32b025fcd288dc37ecf8e694967fa60c934deb3
ddionisio <garapata@gmail.com>
2019-03-16 18:12:29 -0400

    level 5 and 7 anims, dialogs

---
+++ Assets/Scripts/Game/OperationsIntroTripleController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperationsIntroTripleController : MonoBehaviour {
+    [Header("Data")]
+    public MixedNumberOps ops;
+
+    [Header("Widget")]
+    public MixedNumberOpsWidget opsWidget;
+
+    [Header("Signal Listens")]
+    public M8.Signal signalOpen;
+    public M8.Signal signalClose;
+    public SignalBoolean signalAnswerSubmit;
+
+    [Header("Signal Invokes")]
+    public M8.Signal signalCorrect;
+
+    void OnDestroy() {
+        if(signalOpen) signalOpen.callback -= OnSignalOpen;
+        if(signalClose) signalClose.callback -= OnSignalClose;
+
+        if(signalAnswerSubmit) signalAnswerSubmit.callback -= OnSignalAnswerSubmit;
+    }
+
+    void Awake() {
+        opsWidget.gameObject.SetActive(true);
+
+        signalOpen.callback += OnSignalOpen;
+        signalClose.callback += OnSignalClose;
+
+        signalAnswerSubmit.callback += OnSignalAnswerSubmit;
+    }
+
+    void OnSignalOpen() {
+        opsWidget.operation = ops;
+
+        opsWidget.Show();
+    }
+
+    void OnSignalClose() {
+        opsWidget.Hide();
+    }
+    
+    void OnSignalAnswerSubmit(bool isCorrect) {
+        //show hint if incorrect by # amount
+
+        if(isCorrect) {
+            if(signalCorrect)
+                signalCorrect.Invoke();
+        }
+    }
+}
