34d9c16d22496505c108e2002eebc84c7a99550e
David Dionisio <ddionisio@renegadeware.com>
2022-12-06 16:45:32 -0500

    updated lol sdk, auto complete at end, slower attack/defend timer

--- Assets/LoL/Scripts/LoLManagerMockup.cs
+++ Assets/LoL/Scripts/LoLManagerMockup.cs
@@ -5,41 +5,61 @@ using UnityEngine;
 using MiniJSON;
 
 public class LoLManagerMockup : LoLManager {
-    [Header("Mockup")]
-    public TextAsset localizeText;
+	[Header("Mockup")]
+	public TextAsset localizeText;
 
-    protected override void _SpeakText(string key) {
-        
-    }
+	public override bool isAutoSpeechEnabled { get { return false; } }
 
-    public override void ApplyProgress(int progress, int score) {
+	private const string userDataProgKey = "LoLProgress";
+	private const string userDataScoreKey = "LoLScore";
 
-        mCurProgress = Mathf.Clamp(progress, 0, progressMax);
-        
-        ProgressCallback();
-    }
+	protected override void _SpeakText(string key) {
 
-    public override void Complete() {
-        Debug.Log("COMPLETE");
-    }
+	}
 
-    protected override IEnumerator Start() {
-        mLangCode = "en";
-        mCurProgress = 0;
-        
-        ApplySettings();
-                                
-        if(localizeText) {
-            string json = localizeText.text;
+	public override void ApplyProgress(int progress, int score) {
 
-            var langDefs = Json.Deserialize(json) as Dictionary<string, object>;
-            ParseLanguage(Json.Serialize(langDefs[mLangCode]));
-        }
+		mCurProgress = Mathf.Clamp(progress, 0, progressMax);
+		mCurScore = score;
 
-        //ParseGameStart("");
+		if(userData) {
+			userData.SetInt(userDataProgKey, mCurProgress);
+			userData.SetInt(userDataScoreKey, mCurScore);
+			userData.Save();
+		}
 
-        mIsReady = true;
+		ProgressCallback();
+	}
 
-        yield return null;
-    }
+	public override void Complete() {
+		Debug.Log("COMPLETE");
+	}
+
+	protected override IEnumerator Start() {
+		mLangCode = "en";
+		mCurProgress = 0;
+
+		if(userData) {
+			userData.SetMockUp(true);
+			userData.Load();
+
+			mCurProgress = userData.GetInt(userDataProgKey);
+			mCurScore = userData.GetInt(userDataScoreKey);
+		}
+
+		ApplySettings();
+
+		if(localizeText) {
+			string json = localizeText.text;
+
+			var langDefs = Json.Deserialize(json) as Dictionary<string, object>;
+			ParseLanguage(Json.Serialize(langDefs[mLangCode]));
+		}
+
+		//ParseGameStart("");
+
+		mIsReady = true;
+
+		yield return null;
+	}
 }
